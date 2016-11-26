e0d3ed3372aca4453a1a0e17aa6124c9b25223c8
Branden Russell <branden.russell@gmail.com>
2016-11-26 11:01:29 -0700

    Fixed indentation

--- Attributed/UIFont.swift
+++ Attributed/UIFont.swift
@@ -43,7 +43,7 @@ extension UIFont {
                         [
                             UIFontFeatureTypeIdentifierKey: kNumberSpacingType,
                             UIFontFeatureSelectorIdentifierKey: kMonospacedNumbersSelector,
-                            ],
+                        ],
                     ],
                 ]), size: pointSize)
             }
